538399f484be3d54533befe4c2225d807c09db6c
Arman Khan <sparmankhan04@gmail.com>
2026-01-21 13:20:52 +0000

    user data not storing on mongodb

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
-import User from '../../../../models/User.js';
+import User from '@/models/User';
 
 export async function POST(request: Request) {
   try {
